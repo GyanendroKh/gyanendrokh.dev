dff91f9d1bdf509a3a584eb801db06d6ea8eb3e2
Gyanendro Kh <16163526+GyanendroKh@users.noreply.github.com>
2023-01-15 20:35:44 +0530

    Adding Github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,14 @@ const Home: FC = () => {
         <h1 className="text-5xl">Gyanendro Kh</h1>
         <p className="text-base">Every Stack Software Developer 👨🏼‍🔧</p>
         <p className="text-sm">passionate about technology</p>
+        <a
+          className="underline text-blue-500 hover:text-blue-700"
+          href="http://github.com/GyanendroKh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
       </div>
     </div>
   );
